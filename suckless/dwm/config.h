9e91001f2144e662b2884b6329280a8f2405d099
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2021-02-16 12:22:56 +0530

    st

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -57,7 +57,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, NULL };
-static const char *termcmd[]  = { "alacritty" };
+static const char *termcmd[]  = { "st" };
 
 static Key keys[] = {
 	/* modifier             key        function        argument */
