18d050b4cdf9287507ca27a700664272b633d015
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2021-04-19 12:45:08 +0530

    add visrc

---
+++ visrc.lua
@@ -0,0 +1,13 @@
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+	-- Your global configuration options
+	vis:command("set theme base16-nord")
+	vis:command("set autoindent on")
+	vis:command("set tabwidth 4")
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	-- Your per window configuration options e.g.
+	vis:command('set number')
+end)
