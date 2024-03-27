025fe9928bd91c38fa50ad493d2c2937927fb946
git-bruh <prathamIN@proton.me>
2024-03-28 01:10:45 +0530

    .config/vis: add fuzzy grep

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,12 +1,31 @@
 require('vis')
 
+vis:command_register("grep", function()
+    local status, output = vis:pipe("grep --recursive --line-number '' 2>/dev/null | fzy")
+
+    if status == 0 then
+        local filename = string.find(output, ":")
+
+        if filename then
+            local linenumber = string.find(output, ":", filename + 1)
+
+            if linenumber then
+                vis:command(string.format("open %s", string.sub(output, 0, filename - 1)))
+                vis.win.selection:to(tonumber(string.sub(output, filename + 1, linenumber - 1)), 0)
+            end
+        end
+    end
+
+    vis:redraw()
+end, "fuzzy grep")
+
 vis.events.subscribe(vis.events.INIT, function()
     vis:command("set theme catppuccin-mocha")
     vis:command("set autoindent on")
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-    vis:command('set number')
+    vis:command("set number")
     vis:command("set expandtab on")
     vis:command("set tabwidth 4")
 end)
