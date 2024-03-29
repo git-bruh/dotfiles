e39568b8d0856c0a7505e2f1b025c21a506e557d
git-bruh <prathamIN@proton.me>
2024-03-29 23:07:40 +0530

    vis: use git grep and fzy

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -2,7 +2,15 @@ require('vis')
 require('plugins/vis-lspc')
 
 function fuzzyGrep(action)
-    local status, output = vis:pipe("grep --recursive --line-number --exclude-dir=.git '' 2>/dev/null | pick")
+    local gitgrep_args = "git grep -rnI"
+
+    if os.execute("git rev-parse --git-dir >/dev/null 2>&1") == 0 then
+        gitgrep_args = gitgrep_args .. " --untracked"
+    else
+        gitgrep_args = gitgrep_args .. " --no-index"
+    end
+
+    local status, output = vis:pipe(gitgrep_args .. " '' 2>/dev/null | fzy")
 
     if status == 0 then
         local filename = string.find(output, ":")
@@ -31,7 +39,7 @@ end)
 vis.events.subscribe(vis.events.INIT, function()
     vis:command("set theme catppuccin-mocha")
     vis:command("set autoindent on")
-    vis:command("set lspc-menu-cmd 'pick -X'")
+    vis:command("set lspc-menu-cmd fzy")
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
